fb423d7a6a4964d761b3310d81666b718f877d20
matheus frade <codefrads@gmail.com>
2024-08-12 18:43:03 -0300

    feat: responsividade footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,9 +1,10 @@
 import { Instagram, Mail, MapPin, Phone } from "lucide-react";
+import { FaWhatsapp } from "react-icons/fa";
 
 export function Footer() {
-    return (
-        <footer className="flex flex-col md:flex-row justify-center align-middle items-start w-full bg-muted py-8 gap-28 px-4">
-            <div className="flex flex-col space-y-4">
+    return ( 
+        <footer className="flex flex-col md:flex-row justify-center align-middle items-start w-full bg-muted py-8 md:gap-28 px-4">
+            <div className="flex flex-col space-y-4 mb-5">
                 <span className="text-sm font-light text-foreground flex flex-row gap-1 items-center">
                     <div className="rounded-full bg-primary p-1">
                         <MapPin size={18}/> 
@@ -20,7 +21,7 @@ export function Footer() {
                     <div className="rounded-full bg-primary p-1">
                         <Phone size={18}/> 
                     </div>
-                    (21) 96857-8118 / (21) 99257-5323 / (21) 2717-3264
+                    <FaWhatsapp size={15}/>(21) 96857-8118 / <FaWhatsapp size={15}/>(21) 99257-5323 / <Phone size={15}/> (21) 2717-3264
                 </span>
             </div>
             <div className="flex flex-col text-xs gap-8">
@@ -31,14 +32,14 @@ export function Footer() {
                 >
                     <Instagram size={18} />
                 </a>
-                <div className="flex flex-col space-y-2">
+                <div className="flex flex-col space-y-2 md:mt-[0px] mt-[-20px]">
                     <span>CNPJ - 06.876.822/0001-17</span>
                     <span>
                         CEP - 24.730-070
                     </span>
                 </div>
             </div>
-            <div>
+            <div className="absolute md:mt-[120px] mt-[240px] md:ml-[0px] ml-[200px]">
                 <span className="text-xs pt-8">Estrutural Eventos © 2024</span>
             </div>
         </footer>
