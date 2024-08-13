bb78c08be8ea04ac2af82100628851b004d80d6e
JhonnyFrade <joao.guedes2@fatec.sp.gov.br>
2024-08-12 22:07:36 -0300

    ajustando cards

--- src/components/serviceInfo/service.tsx
+++ src/components/serviceInfo/service.tsx
@@ -6,39 +6,39 @@ import { TbBuildingCircus } from "react-icons/tb";
 export function Service() {
     const infos = [
         {
-            Icon: <GiSoundOn size={40} />,
+            Icon: <GiSoundOn size={40} className="text-primary" />,
             title: 'Som',
             info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
         },
         {
-            Icon: <SlScreenDesktop size={40} />,
+            Icon: <SlScreenDesktop size={40} className="text-primary" />,
             title: 'Imagem',
             info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
         },
         {
-            Icon: <FaTheaterMasks  size={40} />,
+            Icon: <FaTheaterMasks  size={40} className="text-primary" />,
             title: 'Palcos',
             info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
         },
         {
-            Icon: <TbBuildingCircus  size={40} />,
+            Icon: <TbBuildingCircus  size={40} className="text-primary" />,
             title: 'Estrutura',
             info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
         },
         {
-            Icon: <FaRegLightbulb size={40} />,
+            Icon: <FaRegLightbulb size={40} className="text-primary" />,
             title: 'Iluminação',
             info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
         },
         {
-            Icon: <BiDotsHorizontalRounded  size={40} />,
+            Icon: <BiDotsHorizontalRounded  size={40} className="text-primary" />,
             title: 'Outros',
             info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
         },
     ];
     return (
         <div className="h-auto w-full flex justify-center mb-10">
-            <main className=" max-w-[1600px] mt-10 flex flex-col items-center pb-8  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
+            <main className=" max-w-[1600px] mt-10 flex flex-col items-center pb-8 border border-gray-200 rounded-lg shadow bg-muted dark:border-gray-700 ">
                 <div className=" flex flex-col items-center mb-10">
                     <h1 className="text-3xl font-bold mb-4 mt-10">Serviços</h1>
                     <p className="text-center max-w-[800px] pl-5 pr-5">
@@ -51,12 +51,12 @@ export function Service() {
                         <a
                             key={index}
                             href="#"
-                            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-blue-200 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-blue-800"
+                            className="block max-w-sm p-6 bg-primary-foreground/20 border border-gray-200 rounded-lg shadow dark:border-gray-700 hover:bg-primary-foreground"
                         >
                             
                             <div>
                             <p className="w-full flex justify-center ">{text.Icon}</p>
-                                <h5 className="w-full flex justify-center mb-2 text-2xl font-bold  tracking-tight text-gray-900 dark:text-white">
+                                <h5 className="w-full flex justify-center mb-2 text-2xl font-bold  tracking-tight text-foreground">
                                     {text.title}
                                 </h5>
                                 <p className="font-normal text-gray-700 dark:text-gray-400">
