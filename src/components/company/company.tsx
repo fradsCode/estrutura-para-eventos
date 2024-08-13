f54c3f412da96eac018bcbdfa7cb1756b9b8db7d
JhonnyFrade <joao.guedes2@fatec.sp.gov.br>
2024-08-12 22:33:33 -0300

    criando componente inicial de empresa

--- src/components/company/company.tsx
+++ src/components/company/company.tsx
@@ -1,20 +1,30 @@
-import { SessionTitle } from "../sessionTitle/sessionTitle";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "../ui/accordion";
 
 export function Company() {
     return (
-        <main className="flex flex-col m-8 h-[30vh] mt-8 space-y-4">
-            <SessionTitle title="Empresa"/>
-            <div className="flex flex-row w-full h-full justify-around">
-                <div className="flex flex-col justify-center items-center rounded-s-full bg-primary w-52"> 
-                    <span className="text-2xl">Quem somos?</span>
-                </div>
-                <div className="w-full border bg-blue-50/5 rounded-e-full ease-in-out animate-pulse hover:bg-blue-50/10">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Corporis explicabo eveniet velit eaque similique temporibus, quisquam dolore fuga impedit, doloribus
-                    </p>
-                </div>
-           </div>
+        <main className="flex flex-col m-8 h-screen mt-8 space-y-4">
+            <div className="w-1/2 flex flex-col space-y-4">
+               <Accordion type="single" collapsible className="border rounded-lg px-4">
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger className="flex justify-start items-baseline gap-2 text-xl">
+                            <span className="text-3xl">10</span> Anos de experiência
+                        </AccordionTrigger>
+                        <AccordionContent>
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+                <Accordion type="single" collapsible className="border rounded-lg px-4">
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger className="flex justify-start items-baseline gap-2 text-xl">
+                            <span className="text-3xl">10</span> Anos de experiência
+                        </AccordionTrigger>
+                        <AccordionContent>
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+            </div>
         </main>
     )
 }
