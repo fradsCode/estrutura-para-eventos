049fbcf8f53818297a2cce603f52648fd352bbcf
matheus frade <codefrads@gmail.com>
2024-08-21 18:20:07 -0300

    feat: texto

--- src/components/company/company.tsx
+++ src/components/company/company.tsx
@@ -7,14 +7,11 @@ export function Company() {
                     <div className="flex flex-col w-full bg-foreground/90 dark:bg-background/90 text-center py-10 px-4 md:px-10 text-white justify-center align-middle items-center space-y-4 rounded-2xl">
                         <h2 className="text-3xl font-bold mt-10">Estrutural Eventos</h2>
                         <p className="text-base text-white leading-6 md:w-3/4 xl:w-2/3 2xl:w-1/3">
-                            Com mais de 20 anos de experiência no mercado, somos a principal empresa de estruturas de palcos no Rio de Janeiro. 
-                            Nossa trajetória é marcada pela excelência, inovação e compromisso com a segurança e a qualidade em cada projeto que realizamos.
+                            Com mais de 20 anos de experiência no mercado, somos a principal empresa de montagem de estruturas para eventos no Rio de Janeiro. Nossa trajetória é marcada pela excelência, inovação e compromisso com a segurança e a qualidade em cada projeto que realizamos.
                         </p>
-                        <h3 className="text-blue-400 font-bold">Quem somos?</h3> 
+                        <h3 className="text-blue-400 font-bold">Quem somos?</h3>
                         <p className="text-base text-foreground text-white leading-6 md:w-3/4 xl:w-2/3 2xl:w-1/3">
-                            Desde nossa fundação, temos nos dedicado a transformar eventos em experiências inesquecíveis. 
-                            Nossa equipe altamente qualificada e nossos equipamentos de ponta garantem a montagem de estruturas robustas e seguras, 
-                            adaptadas às necessidades específicas de cada cliente de forma personalizada.
+                        Desde nossa fundação, temos nos dedicado a transformar eventos em experiências inesquecíveis. Nossa equipe altamente qualificada e nossos equipamentos de ponta garantem a montagem de estruturas robustas e seguras, adaptadas às necessidades específicas de cada cliente de forma personalizada.
                         </p>
                     </div>
                 </div>
