28328a0b0d778dbf6c4c457fc5e869ed9bd97b91
matheus frade <codefrads@gmail.com>
2024-08-21 19:08:11 -0300

    feat: ajuste

--- src/components/whatsIcon/whatsIcon.tsx
+++ src/components/whatsIcon/whatsIcon.tsx
@@ -4,7 +4,11 @@ import '../../../public/logo.png';
 import { Separator } from '../ui/separator';
 export function WhatsIcon() {
   return (
-    <>    
+
+    <>
+          <a href='https://www.instagram.com/estruturaparaeventos/'  target="_blank" className='fixed  right-[28px] bottom-[65px] '>
+            <Image src="/Instagram_icon.png" width={40} height={40} alt='a'/>
+        </a>  
         <a href='https://wa.me/21992575323'  target="_blank" className='fixed  right-0 bottom-0 '>
             <Image src="/whats.png" width={100} height={100} alt='a'/>
         </a>
